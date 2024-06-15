dc34668462f3aa002142a485e2ad4b399b52ddcc
JeeyaSomaiya <144636268+JeeyaSomaiya@users.noreply.github.com>
2024-06-15 15:47:34 -0600

    completed generateChecksum

--- src/util.cpp
+++ src/util.cpp
@@ -5,12 +5,30 @@ Util::Util(QObject *parent)
 {}
 
 QByteArray Util::encodeByteStream(QByteArray bs){
+
     return QByteArray(1, 0x00);
 }
 
 QByteArray Util::generateChecksum(QByteArray bs, int bodyStartIndex, int bodyEndIndex){
+    // ensure bodyEndIndex is greater than or equal to bodyStartIndex, if not, return empty array of bytes
+    if (bodyEndIndex < bodyStartIndex) {
+        return QByteArray(1, 0x00);
+    }
 
-    return QByteArray(1, 0x00);
+    int length = bodyEndIndex - bodyStartIndex + 1;
+
+    // extract the portion of interest from the byte array
+    QByteArray portion = bs.mid(bodyStartIndex, length);
+
+    quint16 checksum = qChecksum(portion.constData(), portion.size());
+
+    // convert checksum to hexadeciaml QByteArray
+    QByteArray checksumHex;
+
+    checksumHex.append(static_cast<char>(checksum & 0xFF));
+    checksumHex.append(static_cast<char>((checksum >> 8) & 0xFF));
+
+    return checksumHex;
 }
 
 QByteArray Util::formatInt(int num, int bytes){
